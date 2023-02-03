80c76fddaeba64b951d2981048809daf0500ab0d
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-02-03 14:58:15 -0500

    Update and rename Main.java to YearlyMaintenanceCost.java

---
+++ src/YearlyMaintenanceCost.java
@@ -0,0 +1,17 @@
+public class YearlyMaintainenaceCost {
+    public static void main(String[] args) {
+    double seasonWinter= 10;
+    double seasonSummer= 20;
+    double seasonFall= 30;
+    double seasonSpring= 40;
+    double total= seasonWinter + seasonSpring + seasonFall + seasonSummer;
+    System.out.println("seasonWinter: $10");
+    System.out.println("seasonSummer: $20");
+    System.out.println("seasonSpring: $30");
+    System.out.println("seasonFall: $40");
+    System.out.println("total= SeasonWinter + seasonSummer + seasonFall + seasonSpring");
+    System.out.println(total);
+
+
+    }
+}
